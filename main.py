3990216491f01afd064286d90cdd099aa0c1de41
nckmcc <nick.mccullough@gmail.com>
2026-07-29 19:06:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -212,5 +212,7 @@ def size_equity(req: EquitySizeRequest):
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
